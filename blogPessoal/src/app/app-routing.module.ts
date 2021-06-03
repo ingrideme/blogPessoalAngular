ff37921e17136700e8eb61ffedeb9addd13d150b
ingrideme <ingrid_melo9@hotmail.com>
2021-06-03 20:51:25 -0300

    Componente Tema

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   
   { path:'', redirectTo: 'entrar', pathMatch: 'full'},
   { path:'entrar', component: EntrarComponent  },
   { path: 'cadastrar', component: CadastrarComponent }, 
-
+  { path: 'tema', component: TemaComponent },
   { path: 'inicio', component:  InicioComponent }
 
 

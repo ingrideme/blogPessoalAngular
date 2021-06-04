d908bdb64ff470d151a68fcd8f2c0eab774a2196
ingrideme <ingrid_melo9@hotmail.com>
2021-06-04 18:07:41 -0300

    Implementando Put e Delete em Tema

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
@@ -8,11 +10,15 @@ import { TemaComponent } from './tema/tema.component';
 const routes: Routes = [
   
   { path:'', redirectTo: 'entrar', pathMatch: 'full'},
+  
   { path:'entrar', component: EntrarComponent  },
   { path: 'cadastrar', component: CadastrarComponent }, 
+  
   { path: 'tema', component: TemaComponent },
-  { path: 'inicio', component:  InicioComponent }
-
+  { path: 'inicio', component:  InicioComponent },
+  
+  { path: 'tema-edit/:id', component: TemaEditComponent },
+  { path: 'tema-delete/:id', component: TemaDeleteComponent }
 
 
 ];
